e0200e9bb86b1c71c54a2f62506c927e8f3f1a1b
yayoitamura <yayoitamura@yayoitamuranoMacBook-Pro.local>
2017-09-08 00:30:58 +0900

    Item作成

---
+++ Assets/_Completed-Assets/Scripts/Item.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour
+{
+
+	void OnTriggerEnter (Collider hit)
+	{
+
+		if (hit.CompareTag ("Player")) {
+
+			Destroy(gameObject);
+		}
+	}
+
+}

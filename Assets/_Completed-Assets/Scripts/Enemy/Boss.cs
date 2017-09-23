ba3fbebb52bda9938303b303d64dd67d3baedf92
yayoitamura <yayoitamura@yayoitamuranoMacBook-Pro.local>
2017-09-23 23:31:48 +0900

    リファクタリング

--- Assets/_Completed-Assets/Scripts/Enemy/Boss.cs
+++ Assets/_Completed-Assets/Scripts/Enemy/Boss.cs
@@ -1,13 +1,13 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class Enemy : MonoBehaviour
+public class Boss : MonoBehaviour
 {
 	public float m_StartingHealth = 1f;               // 開始時の各タンクの体力の値
 	public GameObject m_ExplosionPrefab;                // Awake でインスタンスにされ、その後、タンクが倒されると常に使用されるプレハブ
 
 
-//	private AudioSource m_ExplosionAudio;               // タンクが爆発したときに再生するオーディオ
+	//	private AudioSource m_ExplosionAudio;               // タンクが爆発したときに再生するオーディオ
 	private ParticleSystem m_ExplosionParticles;        // タンクが破壊されたときに再生するパーティクルシステム
 	private float m_CurrentHealth;                      // 現在のタンクの体力値
 	private bool m_Dead;                                // タンクの体力値が 0 を下回ったかどうか
@@ -24,7 +24,7 @@ public class Enemy : MonoBehaviour
 		m_ExplosionParticles = Instantiate (m_ExplosionPrefab).GetComponent<ParticleSystem> ();
 
 		// インスタンスにしたプレハブのオーディオソースへの参照を取得
-//		m_ExplosionAudio = m_ExplosionParticles.GetComponent<AudioSource> ();
+		//		m_ExplosionAudio = m_ExplosionParticles.GetComponent<AudioSource> ();
 
 		// プレハブを無効にし、必要な時に有効にできるようにします。
 		m_ExplosionParticles.gameObject.SetActive (false);
@@ -44,7 +44,7 @@ public class Enemy : MonoBehaviour
 
 		if (other.CompareTag("Player"))
 		{
-//			SlimDamege().SetTrigger("Damage");
+			//			SlimDamege().SetTrigger("Damage");
 			SlimDead().SetTrigger("Dead");
 		}
 	}
@@ -53,7 +53,7 @@ public class Enemy : MonoBehaviour
 	{
 		return animator;
 	}
-		
+
 	public Animator SlimDead()
 	{
 		return animator;
---
+++ Assets/_Completed-Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour
+{
+	//animater
+	private Animator animator;
+
+	private void Awake ()
+	{
+		//animatorの取得
+		animator = GetComponent<Animator> ();
+	}
+
+
+	private void OnEnable()
+	{
+
+	}
+
+
+	void OnTriggerEnter(Collider other) {
+		//砲弾に当たるとDamage再生、削除
+		if (other.CompareTag("Player"))
+		{
+			SlimDamege().SetTrigger("Damage");
+			Destroy (gameObject, 2f);
+		}
+	}
+
+	public Animator SlimDamege()
+	{
+		return animator;
+	}
+
+}

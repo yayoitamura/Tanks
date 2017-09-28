c6913dbe0207ec2e71816ed0eac45588117e1291
yayoitamura <yayoitamura@yayoitamuranoMacBook-Pro.local>
2017-09-28 21:02:16 +0900

    Bossの射撃頻度の修正

--- Assets/_Completed-Assets/Scripts/Enemy/SearchPlayer.cs
+++ Assets/_Completed-Assets/Scripts/Enemy/SearchPlayer.cs
@@ -4,37 +4,83 @@ using System.Collections;
 using UnityEngine.AI;
 
 public class SearchPlayer : MonoBehaviour {
-    
+
+	public Rigidbody m_Shell;                   // Prefab of the shell.
+	public Transform m_FireTransform;
+	private float m_CurrentLaunchForce = 5;
+    private bool flag = true;
+
 //    public Transform target;
 	private NavMeshAgent agent;
 
-	void Start () {
+	//void Start () {
+	//	agent = GetComponent<NavMeshAgent>();
+		//InvokeRepeating("Atack", 2f, 3f);
+	//}
+
+	IEnumerator Start()
+	{
 		agent = GetComponent<NavMeshAgent>();
-	}
 
-	void Update () {
-        //Vector3 target = GameObject.FindGameObjectWithTag("Player").transform.position;		
-        //agent.destination = target;
-
-	}
-
-	//BossのPlayer探索範囲につけた当たり判定
-	void OnTriggerStay(Collider other) {
-		Vector3 target = GameObject.FindGameObjectWithTag("Player").transform.position;
-
-		if (other.CompareTag("Player")){
-		agent.destination = target;
-
-		//Destroy(gameObject);
-		//isDetectPlayer = true;
-		//lookTarget = other.transform;
+		while (true)
+		{
+            Atack();
+			yield return new WaitForSeconds(1f);
 		}
 	}
 
-	//void OnTriggerExit(Collider other){
-	//	if (other.CompareTag("Player")) {
-	//		//isDetectPlayer = false;
-	//		//lookTarget = null;
-	//	}
-	//}
+    void Update () {
+
+	}
+
+    private void Atack() {
+        if (flag == true)
+        {
+            Rigidbody shellInstance =
+            Instantiate(m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
+            shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward;
+        }
+	}
+
+	//BossのPlayer探索範囲につけた当たり判定
+    void OnTriggerStay(Collider other) {
+		Vector3 target = GameObject.FindGameObjectWithTag("Player").transform.position;
+
+        if (other.CompareTag("Player")){ 
+
+			//追跡
+			agent.destination = target;
+            flag = true;
+
+			// 砲弾のインスタンスを作成して、そのリジッドボディへの参照を格納
+			//Rigidbody shellInstance =
+			//	Instantiate(m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
+
+			// 射撃位置の前方方向で、砲弾速度を発射力に設定
+			//shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward;
+
+        } else {
+            flag = false;
+        }
+ 	}
+
 }
+
+
+
+/*
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ *
+ *
+ * 
+ * 
+ * 
+ */

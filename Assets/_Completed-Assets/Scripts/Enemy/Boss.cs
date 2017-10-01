74db10b1a95465fa6d0577357a404c99503ea27f
yayoitamura <yayoitamura@yayoitamuranoMacBook-Pro.local>
2017-10-01 16:26:25 +0900

    Bossの体力スライダーを表示

--- Assets/_Completed-Assets/Scripts/Enemy/Boss.cs
+++ Assets/_Completed-Assets/Scripts/Enemy/Boss.cs
@@ -3,18 +3,23 @@ using UnityEngine.UI;
 
 public class Boss : MonoBehaviour
 {
-	public float m_StartingHealth = 1f;               // 開始時の各タンクの体力の値
+	public float m_StartingHealth = 100f;                 // 開始時の各タンクの体力の値
+	public Slider m_Slider;                             // 現在のタンクの体力を示すスライダー
+	public Image m_FillImage;                           // スライダーの Image コンポーネント
+	public Color m_FullHealthColor = Color.green;       // The color the health bar will be when on full health.
+	public Color m_ZeroHealthColor = Color.red;
 	public GameObject m_ExplosionPrefab;                // タンクが倒されると使用されるプレハブ
 
 
 	//	private AudioSource m_ExplosionAudio;               // タンクが爆発したときに再生するオーディオ
 	private ParticleSystem m_ExplosionParticles;        // タンクが破壊されたときに再生するパーティクルシステム
 	private float m_CurrentHealth;                      // 現在のタンクの体力値
-	private bool m_Dead;                                // タンクの体力値が 0 を下回ったかどうか
+    public bool BossDestroy;
 
 	private void Awake ()
 	{
 
+        //BossDestroy = false;
 
 		//爆発のプレハブをインスタンスにして、そのパーティクルシステムへの参照を取得
 		m_ExplosionParticles = Instantiate (m_ExplosionPrefab).GetComponent<ParticleSystem> ();
@@ -31,8 +36,19 @@ public class Boss : MonoBehaviour
 	{
 		// タンクが有効にされるとき、タンクの体力をリセットし、倒されていない状態にリセットします。
 		m_CurrentHealth = m_StartingHealth;
-		m_Dead = false;
+        BossDestroy = false;
+        SetHealthUI();
+	}
 
+	private void SetHealthUI()
+	{
+		// スライダーに適切な値を設定
+		// 開始時に対する現在の体力のパーセントに基づいて、選択した色でバーを満たします。
+		// Set the slider's value appropriately.
+		m_Slider.value = m_CurrentHealth;
+
+		// Interpolate the color of the bar between the choosen colours based on the current percentage of the starting health.
+		m_FillImage.color = Color.Lerp(m_ZeroHealthColor, m_FullHealthColor, m_CurrentHealth / m_StartingHealth);
 	}
 
 
@@ -63,9 +79,11 @@ public class Boss : MonoBehaviour
 		Debug.Log(other.tag + "tag");
 
 		//砲弾に当たるとDamage再生、削除
-		if (other.CompareTag("Player"))
+		if (other.CompareTag("Shell"))
 		{
-			Destroy(gameObject, 2f);
+			BossDestroy = true;
+
+            Destroy(gameObject.transform.parent.gameObject);
 		}
 	}
 
